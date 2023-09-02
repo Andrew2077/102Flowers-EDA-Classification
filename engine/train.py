dfba9b878e122941b91316155ec8dd6f3654aa3e
Andrew Muhsen Paramedis <Andrew.naaem99@eng-st.cu.edu.eg>
2023-09-02 21:15:27 +0300

    .

--- engine/train.py
+++ engine/train.py
@@ -149,14 +149,7 @@ def training_loop(
         print(
             "---------------------------------------------------------------------------------------------------------------------"
         )
-        #* ******************** GradCAM ********************#
-        
-        fig, ax = gradcam.plot_grad_cam(test_tensor, test_target.item())
-        # fig.savefig(f"results/gradcam_{epoch+1}.png")
-        # #* show figure
-        plt.title(f'gradcam-Hisroy at Epoch : {epoch+1}')
-        plt.show()
-        
+
         # ******************** tensorboard********************#
         # * Tensorboard scalars
         writer.add_scalars(
@@ -181,6 +174,14 @@ def training_loop(
         #     model=model,
         #     input_to_model=torch.rand(64, 3, 224, 224).to(device),
         # )
+        
+        #* ******************** GradCAM ********************#
+        
+        fig, ax = gradcam.plot_grad_cam(test_tensor, test_target.item())
+        # fig.savefig(f"results/gradcam_{epoch+1}.png")
+        # #* show figure
+        plt.title(f'gradcam-Hisroy at Epoch : {epoch+1}')
+        plt.show()
         
         
     writer.close()
